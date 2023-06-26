0f0bffeb0a13f769639d7d9f9ac27324e646541a
sako590829 <sako590829@yahoo.co.jp>
2023-06-26 22:56:46 +0900

    修正

--- guraduaetionwork/models.py
+++ guraduaetionwork/models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
 
 
 class Post(models.Model):
@@ -16,3 +21,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Sample(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
